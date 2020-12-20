aabb5f01a1837a89c3ff0f86536c4216c2a82bf5
Marcus Fihlon <marcus@fihlon.swiss>
2020-12-20 18:12:30 +0100

    :card_file_box: Adding MySQL support

--- src/main/java/swiss/fihlon/pensum/backend/service/UserService.java
+++ src/main/java/swiss/fihlon/pensum/backend/service/UserService.java
@@ -18,9 +18,6 @@
  */
 package swiss.fihlon.pensum.backend.service;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 import swiss.fihlon.pensum.backend.entity.User;
 import swiss.fihlon.pensum.backend.repository.UserRepository;
@@ -36,16 +33,6 @@ public class UserService {
 
     public User findByEmail(final String email) {
         return userRepository.findByEmail(email);
-    }
-
-    @PostConstruct
-    public void populateTestData() {
-        if (userRepository.count() == 0) {
-            userRepository.saveAll(
-                    Stream.of(
-                            new User("user", "{noop}password")
-                    ).collect(Collectors.toList()));
-        }
     }
 
 }
