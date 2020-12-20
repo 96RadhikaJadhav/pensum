4f115c033cc157845746ab97291b7fc1f22b9902
Marcus Fihlon <marcus@fihlon.swiss>
2020-12-20 13:47:09 +0100

    :page_facing_up: Adding copyright and license note to Java source files

--- src/main/java/swiss/fihlon/pensum/views/main/MainView.java
+++ src/main/java/swiss/fihlon/pensum/views/main/MainView.java
@@ -1,3 +1,21 @@
+/*
+ * Pensum
+ * Copyright © 2020 Marcus Fihlon
+ * https://github.com/pensum/pensum
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package swiss.fihlon.pensum.views.main;
 
 import com.vaadin.flow.component.Component;
@@ -20,9 +38,6 @@ import java.util.Optional;
 import swiss.fihlon.pensum.views.about.AboutView;
 import swiss.fihlon.pensum.views.logout.LogoutView;
 
-/**
- * The main view is a top-level placeholder for other views.
- */
 @JsModule("./styles/shared-styles.js")
 @CssImport("./styles/views/main/main-view.css")
 public class MainView extends AppLayout {
